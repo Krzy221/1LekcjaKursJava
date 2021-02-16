2689068555ea5b4bdd35aeb01962f19bdf7c1aa2
Krzy221 <79155912+Krzy221@users.noreply.github.com>
2021-02-16 19:05:26 +0100

    Spacje

--- src/App.java
+++ src/App.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
      
-            /*   System.out.println("Podaj swoje imię");
-            Scanner scanner = new Scanner(System.in); 
-            String firstName = scanner.nextLine();
+              /*   System.out.println("Podaj swoje imię");
+             Scanner scanner = new Scanner(System.in); 
+             String firstName = scanner.nextLine();
                 
-            System.out.println("Twoje imię to " + firstName);
-            scanner.close();
-            */
-             /*
-                Scanner scanner = new Scanner(System.in); 
-            
-                System.out.println("Podaj pierwszą liczbe");
-                int a = scanner.nextInt();
-            
-                System.out.println("Podaj drugą liczbe");
-                int b = scanner.nextInt();
-                
-                System.out.println("Podaj trzecią liczbe");
-                int c = scanner.nextInt();
-                
-                System.out.println("Wynik: " + (a + b + c));
+             System.out.println("Twoje imię to " + firstName);
              scanner.close();
-              */
-   
+             */
+                 /*
+                    Scanner scanner = new Scanner(System.in); 
+                
+                    System.out.println("Podaj pierwszą liczbe");
+                    int a = scanner.nextInt();
+                
+                    System.out.println("Podaj drugą liczbe");
+                    int b = scanner.nextInt();
+                    
+                    System.out.println("Podaj trzecią liczbe");
+                    int c = scanner.nextInt();
+                    
+                    System.out.println("Wynik: " + (a + b + c));
+                 scanner.close();
+                 */
+    
             
               Scanner scanner = new Scanner(System.in); 
             
@@ -42,13 +42,13 @@ public class App {
               int b = scanner.nextInt();
               
 
-            if(o.equals("+")){
-            System.out.println("Wynik to "+ (a+b));
-            }else if(o.equals("-")){
-            System.out.println("Wynik to "+ (a-b));
-            }else{
-            System.out.println("Złe dane");
-            }
-            scanner.close();
+             if(o.equals("+")){
+             System.out.println("Wynik to "+ (a+b));
+             }else if(o.equals("-")){
+             System.out.println("Wynik to "+ (a-b));
+             }else{
+             System.out.println("Złe dane");
+             }
+             scanner.close();
     }
 }
